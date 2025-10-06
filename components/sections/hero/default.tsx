a3dab23d030ebfb61894fa6442c576b3cc959ec3
Arnob-B <arnobbhakta00@gmail.com>
2025-10-06 21:12:01 +0530

    feat(hero): add text alignment options and disable buttons
    - disable buttons from props
    - align text to left or center

--- components/sections/hero/default.tsx
+++ components/sections/hero/default.tsx
@@ -29,6 +29,7 @@ interface HeroProps {
   badge?: ReactNode | false;
   buttons?: HeroButtonProps[] | false;
   className?: string;
+	textAlignment?:"left" | "center";
 }
 
 export default function Hero({
@@ -55,20 +56,21 @@ export default function Hero({
       </a>
     </Badge>
   ),
-  buttons = [
-    {
-      href: siteConfig.getStartedUrl,
-      text: "Get Started",
-      variant: "default",
-    },
-    {
-      href: siteConfig.links.github,
-      text: "Github",
-      variant: "glow",
-      icon: <Github className="mr-2 size-4" />,
-    },
-  ],
+  // buttons = [
+  //   {
+  //     href: siteConfig.getStartedUrl,
+  //     text: "Get Started",
+  //     variant: "default",
+  //   },
+  //   {
+  //     href: siteConfig.links.github,
+  //     text: "Github",
+  //     variant: "glow",
+  //     icon: <Github className="mr-2 size-4" />,
+  //   },
+  // ],
   className,
+	textAlignment = "center"
 }: HeroProps) {
   return (
     <Section
@@ -78,7 +80,9 @@ export default function Hero({
       )}
     >
       <div className="max-w-container mx-auto flex flex-col gap-12 pt-16 sm:gap-24">
-        <div className="flex flex-col items-center gap-6 text-center sm:gap-12">
+        <div className={`flex flex-col 
+					items-${textAlignment ==="left"? "start": "center"} gap-6 
+					text-${textAlignment} sm:gap-12`}>
           {badge !== false && badge}
           <h1 className="animate-appear from-foreground to-foreground dark:to-muted-foreground relative z-10 inline-block bg-linear-to-r bg-clip-text text-4xl leading-tight font-semibold text-balance text-transparent drop-shadow-2xl sm:text-6xl sm:leading-tight md:text-8xl md:leading-tight">
             {title}
